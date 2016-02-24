ae2b938fbf45bf65292e5b525f2b7292d8212a90
Josh <deltaplaythroughs65@gmail.com>
2016-02-24 09:41:44 -0800

    Began rewriting with new Traits functionality.

--- Memory/Allocators/Include/segregator.h
+++ Memory/Allocators/Include/segregator.h
@@ -1,5 +1,36 @@
-//#pragma once
-//
+#pragma once
+
+/*Custom library*/
+#include "..\..\Internal\Include\traits.h"
+
+#include "base_allocator.h"
+
+template<typename threshold,
+	typename SmallAllocator,
+	typename LargeAllocator>
+class Segregator
+	:	private SmallAllocator,
+		private LargeAllocator
+{
+public:
+	Segregator							(void);
+	virtual								~Segregator(void) override;
+
+	virtual Block& allocate				(size_t size) override;
+	virtual bool allocateAll			(size_t size) override;
+	virtual bool deallocate				(Block& block) override;
+	virtual bool deallocateAll			(void) override;
+	virtual bool reallocate				(Block& block, size_t newSize) override;
+	virtual bool realloateAll			(size_t newSize) override;
+	virtual bool owns					(Block block) override;
+	virtual void expand					(size_t amount) override;
+
+private:
+	Segregator					(const Segregator&) = delete;
+	Segregator					(Segregator&&) = delete;
+	Segregator& operator=		(Segregator) = delete;
+};
+
 ///*
 //==================================
 //
