058e587ad00ce0692c6bedcdd3e6bebecfca89a1
Josh <deltaplaythroughs65@gmail.com>
2016-03-01 10:11:00 -0800

    Refactored and add more functions

--- Math/Include/math.h
+++ Math/Include/math.h
@@ -1,14 +1,30 @@
 #pragma once
 
+#include "..\..\Core\common.h"
+
 namespace math
 {
-	template<typename Type>
-	Type abs(Type x)
+	inline float64 abs(float64 x)
 	{
 		return((x < 0) ? -x : x);
 	}
 
-	double inline __declspec (naked) __fastcall sqrt(double n)
+	inline float64 pow(float64 x, int32 p)
+	{
+		float64 n = x;
+		for (int32 i = 1; i < math::abs(p); i++)
+		{
+			n *= x;
+		}
+		return(n);
+	}
+
+	inline float64 squared(float64 x)
+	{
+		return(x * x);
+	}
+
+	inline float64 __declspec (naked) __fastcall sqrt(float64 n)
 	{
 		_asm fld qword ptr[esp + 4]
 			_asm fsqrt
--- Math/Include/vector.hpp
+++ Math/Include/vector.hpp
@@ -1,102 +1,25 @@
 #pragma once
 
-#include "..\..\Core\common.h"
+#include "math.h"
 
 namespace math
 {
-	template<typename Type>
-	struct VectorBase
+	class Vector2
 	{
-		typedef Type Value;
-		typedef Type& Reference;
-		typedef const Type& ConstReference;
-		typedef float32 Length;
+	public:
+		inline Vector2();
+		inline Vector2(float32 x, float32 y);
+		inline Vector2(const Vector2& vec);
 
-		virtual Reference operator[](size_t i) = 0;
-		virtual Length length() = 0;
+		inline void operator+(const Vector2& vec);
+		inline void operator-(const Vector2& vec);
+		inline void operator==(const Vector2& vec);
+		inline void operator!=(const Vector2& vec);
+
+		inline bool equals(const Vector2& other);
+
+	private:
+		float32 _x;
+		float32 _y;
 	};
-
-	template<typename Type = int32>
-	struct Vector2
-		: public VectorBase<Type>
-	{
-
-		virtual Reference operator[](size_t i) override
-		{
-			switch (i)
-			{
-			case 0:
-				return(x);
-			case 1:
-				return(y);
-			default:
-				return(Value());
-			}
-		}
-		virtual Length length() override
-		{
-			static float32 length;
-
-		}
-
-		Value x;
-		Value y;
-	};
-
-	template<typename Type = int32>
-	struct Vector3
-		: public VectorBase<Type>
-	{
-
-		virtual operator[](size_t i) override;
-		virtual Length length() override;
-
-		Value x;
-		Value y;
-		Value z;
-	};
-
-	template<typename Type = int32>
-	struct Vector4
-		: public VectorBase<Type>
-	{
-		
-		virtual operator[](size_t i) override;
-		virtual Length length() override;
-
-		Value x;
-		Value y;
-		Value z;
-		Value w;
-	};
-
-	typedef int32			 Scalari;
-	typedef Vector2<int32>	 Vector2i;
-	typedef Vector3<int32>	 Vector3i;
-	typedef Vector4<int32>	 Vector4i;
-
-	typedef uint32			 Scalarui;
-	typedef Vector2<uint32>  Vector2ui;
-	typedef Vector3<uint32>	 Vector3ui;
-	typedef Vector4<uint32>  Vector4ui;
-
-	typedef int64			 Scalarl;
-	typedef Vector2<int64>	 Vector2l;
-	typedef Vector3<int64>	 Vector3l;
-	typedef Vector4<int64>	 Vector4l;
-
-	typedef uint64			 Scalarul;
-	typedef Vector2<uint64>  Vector2ul;
-	typedef Vector3<uint64>  Vector3ul;
-	typedef Vector4<uint64>  Vector4ul;
-
-	typedef float32			 Scalarf;
-	typedef Vector2<float32> Vector2f;
-	typedef Vector3<float32> Vector3f;
-	typedef Vector4<float32> Vector4f;
-
-	typedef float64			 Scalard;
-	typedef Vector2<float64> Vector2d;
-	typedef Vector3<float64> Vector3d;
-	typedef Vector3<float64> Vector4d;
 }
