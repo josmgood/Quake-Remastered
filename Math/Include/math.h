892d29823d7536679a01173a0dc783b40153867f
Josh <deltaplaythroughs65@gmail.com>
2016-03-06 11:39:41 -0800

    Added log2 and GCD functions.

--- Math/Include/math.h
+++ Math/Include/math.h
@@ -2,6 +2,8 @@
 
 #include "..\..\Core\Include\common.h"
 
+#define PI = 3.14159265358979323846
+
 namespace math
 {
 	inline float64 abs(float64 x)
@@ -22,6 +24,28 @@ namespace math
 	inline float64 squared(float64 x)
 	{
 		return(x * x);
+	}
+
+	inline int32 log2(int32 val)
+	{
+		int32 answer = 0;
+		while (val >>= 1)
+		{
+			answer++;
+		}
+		return(answer);
+	}
+
+	inline int32 GCD(int32 one, int32 two)
+	{
+		if (one > two)
+		{
+			return((two == 0) ? one : GCD(two, one % two));
+		}
+		else
+		{
+			return((one == 0) ? two : GCD(one, two % one));
+		}
 	}
 
 	inline float64 __declspec (naked) __fastcall sqrt(float64 n)
