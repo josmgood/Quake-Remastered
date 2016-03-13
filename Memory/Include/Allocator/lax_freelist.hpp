c0276fd9a4aacc8d2ccdc05a9d8637ee83837924
Josh <deltaplaythroughs65@gmail.com>
2016-03-12 17:38:13 -0800

    Added some of the implementation

--- Memory/Include/Allocator/lax_freelist.hpp
+++ Memory/Include/Allocator/lax_freelist.hpp
@@ -3,11 +3,17 @@
 #include "freelist.hpp"
 
 template<size_t min,
-	size_t max,
-	size_t capacity = DEFAULT_ALLOCATOR_CAPACITY>
+	size_t max>
 class LaxFreeList
-	: public FreeList<capacity>
 {
-public:
-	LaxFreeList();
+	inline LaxFreeList(size_t capacity);
+	inline ~LaxFreeList();
+
+	inline Block allocate(size_t size);
+	inline void deallocate(Block& block);
+	inline QBool owns(Block block) const;
+	void destroy();
+private:
+	Node* _getNext(Node* current);
+	void _setNext(Node* current, Node* next);
 };
