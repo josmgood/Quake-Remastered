3bf53c8f0ab4e4e9fa9d50830550766192174f0b
Josh <deltaplaythroughs65@gmail.com>
2016-03-13 14:27:37 -0700

    Added constructor safety.

--- Memory/Include/Allocator/pool_allocator.hpp
+++ Memory/Include/Allocator/pool_allocator.hpp
@@ -9,53 +9,6 @@
 
 #include "..\..\..\Math\Include\math.h"
 
-//template<typename TBlock>
-//class PoolAllocator
-//{
-//public:
-//	//struct BlockInfo
-//	//{
-//	//	BlockInfo(Address addr = nullptr, size_t i = 0);
-//	//	Address address;
-//	//	size_t index;
-//
-//	//	operator QBool() const;
-//	//};
-//
-//	inline PoolAllocator(size_t capacity = DEFAULT_ALLOCATOR_CAPACITY);
-//	inline ~PoolAllocator();
-//
-//	inline Block allocate(size_t size);
-//	inline void deallocate(Block& block);
-//	inline QBool owns(Block block) const;
-//	inline void expand(size_t size);
-//	inline void destroy();
-//
-//	inline size_t getCapacity() const;
-//	inline size_t getMemoryUsed() const;
-//	inline size_t getBlockSize() const;
-//	inline size_t getNumBlocks() const;
-//	inline size_t getPadding() const;
-//	inline size_t getTrueSize() const;
-//private:
-//	/*QBool _isFree(size_t index) const;
-//	QBool _isAllocated(size_t index) const;
-//
-//	BlockInfo _findFree() const;*/
-//
-//	//internal::Byte* _memory;
-//	//Block* _blocks;
-//	//internal::QBoolSet _flags;
-//	FreeList _freeList;
-//
-//	size_t _capacity;
-//	size_t _memoryUsed;
-//	size_t _blockSize;
-//	size_t _numBlocks;
-//	size_t _padding;
-//	size_t _trueSize;
-//};
-
 template<typename TBlock>
 class PoolAllocator
 {
@@ -80,7 +33,7 @@ private:
 	QBool _isFree(size_t index) const;
 	QBool _isAllocated(size_t index) const;
 
-	Block& _findBlock(QBool flag = false);
+	Block _findBlock();
 
 	size_t _capacity;
 	size_t _memoryUsed;
@@ -90,9 +43,20 @@ private:
 	size_t _trueSize;
 	size_t _padding;
 	size_t _blockSize;
-	FreeList _freeList;
 	internal::BoolSet _flags;
 	QBool _canExpand;
+
+	PoolAllocator(PoolAllocator&) = delete;
+	PoolAllocator(const PoolAllocator&) = delete;
+	PoolAllocator(PoolAllocator&&) = delete;
+	PoolAllocator operator=(PoolAllocator&) = delete;
+	PoolAllocator& operator=(const PoolAllocator&) = delete;
+	PoolAllocator* operator&() = delete;
+
+	void* operator new(size_t) = delete;
+	void* operator new[](size_t) = delete;
+	void operator delete(void*) = delete;
+	void operator delete[](void*) = delete;
 };
 
 #include "..\..\Source\Allocator\pool_allocator.inl"
