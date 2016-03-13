eef88cb45e2ddef40868aa48c8e890a02560d431
Josh <deltaplaythroughs65@gmail.com>
2016-03-12 23:23:45 -0800

    Initial Commit

---
+++ Container/Include/qbool.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class QBool
+{
+public:
+	QBool(bool b = false);
+
+	void setTrue();
+	void setFalse();
+	void set(bool bl);
+	bool get() const;
+
+	bool isTrue() const;
+	bool isFalse() const;
+
+	operator bool() const;
+	void operator=(bool b);
+	//bool operator==(const QBool& qbool) const;
+	bool operator!=(const QBool& qbool) const;
+	bool operator<(const QBool& qbool) const;
+	bool operator>(const QBool& qbool) const;
+
+	std::string toString();
+private:
+	bool _bool;
+};
---
+++ Container/Source/qbool.cpp
@@ -0,0 +1,77 @@
+#include "..\Include\qbool.h"
+
+QBool::QBool(bool b)
+	: _bool(b)
+{
+}
+
+void QBool::setTrue()
+{
+	_bool = true;
+}
+
+void QBool::setFalse()
+{
+	_bool = false;
+}
+
+void QBool::set(bool b)
+{
+	_bool = b;
+}
+
+bool QBool::get() const
+{
+	return(_bool);
+}
+
+bool QBool::isTrue() const
+{
+	return(_bool == true);
+}
+
+bool QBool::isFalse() const
+{
+	return(_bool == false);
+}
+
+QBool::operator bool() const
+{
+	return(_bool);
+}
+
+void QBool::operator=(bool b)
+{
+	_bool = b;
+}
+
+//bool QBool::operator==(const QBool& qbool) const
+//{
+//	return(_bool == qbool._bool);
+//}
+
+bool QBool::operator!=(const QBool& qbool) const
+{
+	return(_bool != qbool._bool);
+}
+
+bool QBool::operator<(const QBool& qbool) const
+{
+	return(_bool < qbool._bool);
+}
+
+bool QBool::operator>(const QBool& qbool) const
+{
+	return(_bool > qbool._bool);
+}
+
+std::string QBool::toString()
+{
+	switch (_bool)
+	{
+	case true:
+		return("True");
+	case false:
+		return("False");
+	}
+}
