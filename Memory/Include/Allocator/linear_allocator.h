9f4c77cfe5c5b560083d4b73dcc8acaf5d71661a
Josh <deltaplaythroughs65@gmail.com>
2016-03-13 14:27:18 -0700

    Added method declarations.

--- Memory/Include/Allocator/linear_allocator.h
+++ Memory/Include/Allocator/linear_allocator.h
@@ -5,18 +5,27 @@
 
 #include "base_allocator.h"
 
+#define MIN_FRAG = 64
 
-
-template<size_t minBlockSize, size_t maxBlockSize, size_t minFrag>
+template<size_t minBlockSize, size_t maxBlockSize, size_t minFrag = MIN_FRAG>
 class LinearAllocator
 {
 public:
-	LinearAllocator(size_t size);
-	LinearAllocator();
-	~LinearAllocator();
-
-	Block allocate(size_t size);
-	void deallocate(Block& block);
+	inline LinearAllocator(size_t capacity, QBool expandStatus = false);
 private:
-	Block _memory;
+	internal::Byte* _memory;
+	Block* _blocks;
+	size_t _numBlocks;
+
+	LinearAllocator(LinearAllocator&) = delete;
+	LinearAllocator(const LinearAllocator&) = delete;
+	LinearAllocator(LinearAllocator&&) = delete;
+	LinearAllocator operator=(LinearAllocator&) = delete;
+	LinearAllocator& operator=(const LinearAllocator&) = delete;
+	LinearAllocator* operator&() = delete;
+
+	void* operator new(size_t) = delete;
+	void* operator new[](size_t) = delete;
+	void operator delete(void*) = delete;
+	void operator delete[](void*) = delete;
 };
