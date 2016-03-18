980992d8e400431df50898fae3ad34353b374e60
Josh <deltaplaythroughs65@gmail.com>
2016-03-17 22:01:49 -0700

    Implemented more methods.

--- Container/Include/qstring.h
+++ Container/Include/qstring.h
@@ -4,12 +4,32 @@
 
 #include "base.hpp"
 #include "..\..\Auxiliary\Include\string_aux.h"
+#include "..\..\Math\Include\math.h"
 #include "..\..\Memory\Include\Allocator\pool_allocator.hpp"
 
 #define DEFAULT_STRING_SIZE		32
-#define EMPTY_STRING QString(0);
+#define EMPTY_STRING QString((size_t)0);
 
-static char EMPTY_CHAR = ' ';
+enum STRING_SEARCH_CASE_SENSITIVITY
+{
+	SENSITIVE = 0,
+	UNSENSITIVE = 1
+};
+
+enum STRING_SEARCH_STARTING_POINT
+{
+	BEGINNING = 0,
+	ENDING = 1
+};
+
+typedef STRING_SEARCH_CASE_SENSITIVITY Sensitivity;
+typedef STRING_SEARCH_STARTING_POINT Start;
+
+QBool startBeginning(Start start);
+QBool startEnding(Start start);
+
+QBool isSensitive(Sensitivity sensitivity);
+QBool isUnsensitivity(Sensitivity sensitivity);
 
 class QString
 	: public QAuxiliary<char, int>
@@ -41,11 +61,19 @@ public:
 	QString substring(size_t begin, size_t end);
 	//QString substring();
 
-	size_t find(Character ch);
-	size_t findLast(Character ch);
+	size_t find(Character ch, Sensitivity sensitivty = UNSENSITIVE, Start start = BEGINNING);
+	size_t findLast(Character ch, Sensitivity sensitivity = UNSENSITIVE, Start start = BEGINNING);
+	QBool has(Character ch, Sensitivity sensitivity = UNSENSITIVE, Start start = BEGINNING);
+
+	QString toUpper() const;
+	void setUpper();
+	void setUpper(size_t index);
+	void setUpper(size_t begin, size_t end);
 
 	QString toLower() const;
-	QString toUpper() const;
+	void setLower();
+	void setLower(size_t index);
+	void setLower(size_t begin, size_t end);
 
 	void copy(const QString& string);
 
@@ -56,9 +84,9 @@ public:
 	bool isFull() const;
 	void pack();
 	void swap(QString& other);
-	bool owns(Character ch);
 
 	Reference at(size_t index) const;
+	QString at(size_t begin, size_t end) const;
 	Reference operator[](size_t index) const;
 
 	friend std::ostream& operator<<(std::ostream& os, const QString& string);
@@ -70,23 +98,10 @@ public:
 private:
 	void _setLength(size_t len);
 
+	QBool _checkIndex(size_t index) const;
+
 	void _incrementLength();
 	void _decrementLength();
-
-	enum SEARCH_CASE_SENSITIVITY
-	{
-		/*Characters must have exact casing*/
-		SENSITIVE,
-		/*Character casing is irrelevant.*/
-		UNSENSITIVE
-	};
-	enum SEARCH_STARTING_POINT
-	{
-		/*Start searching the string at the beginning (forward)*/
-		BEGINNING,
-		/*Start searching the string at the end (backward)*/
-		END
-	};
 
 	/*Raw string*/
 	String _string;
--- Container/Source/qstring.cpp
+++ Container/Source/qstring.cpp
@@ -1,5 +1,25 @@
 #include "..\Include\qstring.h"
 
+QBool startBeginning(Start start)
+{
+	return start == STRING_SEARCH_STARTING_POINT::BEGINNING;
+}
+
+QBool startEnding(Start start)
+{
+	return start == STRING_SEARCH_STARTING_POINT::ENDING;
+}
+
+QBool isSensitive(Sensitivity sensitivity)
+{
+	return sensitivity == STRING_SEARCH_CASE_SENSITIVITY::SENSITIVE;
+}
+
+QBool isUnsensitivity(Sensitivity sensitivity)
+{
+	return sensitivity == STRING_SEARCH_CASE_SENSITIVITY::UNSENSITIVE;
+}
+
 QString::QString(size_t length)
 	: _maxLength(length), _length()
 {
@@ -97,12 +117,142 @@ QString QString::substring(size_t begin)
 		qStrCpy(start, string._string);
 		return string;
 	}
+	return EMPTY_STRING;
+}
 
+size_t QString::find(Character ch, Sensitivity sensitivity, Start start)
+{
+	return 0;
+}
+
+QBool QString::has(Character ch, Sensitivity sensitivity, Start start)
+{
+	size_t begin = (startBeginning(start)) ? 0 : _length - 1;
+	size_t end = (startBeginning(start)) ? _length - 1 : 0;
+	QBool sensitive = isSensitive(sensitivity);
+	for (size_t i = begin; i != end; i++)
+	{
+		if (sensitive)
+		{
+			if (qChrCaseCmp(ch, _string[i]))
+			{
+				return true;
+			}
+		}
+		else
+		{
+
+		}
+	}
+	return false;
+}
+
+QString QString::toUpper() const
+{
+	QString upper(_length);
+	upper._length = _length;
+	for (size_t i = 0; i < _length; i++)
+	{
+		upper[i] = toupper(_string[i]);
+	}
+	return upper;
+}
+
+void QString::setUpper()
+{
+	for (size_t i = 0; i < _length; i++)
+	{
+		Character upper = toupper(_string[i]);
+		_string[i] = upper;
+	}
+}
+
+void QString::setUpper(size_t index)
+{
+	Character ch = at(index);
+	if (!isEmptyChar(ch))
+	{
+		_string[index] = toupper(ch);
+	}
+}
+
+void QString::setUpper(size_t begin, size_t end)
+{
+	QBool begCheck = _checkIndex(begin);
+	QBool endCheck = _checkIndex(end);
+	if (begCheck && endCheck)
+	{
+		for (size_t i = begin; i < end + 1; i++)
+		{
+			Character upper = toupper(_string[i]);
+			_string[i] = upper;
+		}
+	}
+}
+
+QString QString::toLower() const
+{
+	QString lower(_length);
+	lower._length = _length;
+	for (size_t i = 0; i < _length; i++)
+	{
+		lower[i] = tolower(_string[i]);
+	}
+	return lower;
+}
+
+void QString::setLower()
+{
+	for (size_t i = 0; i < _length; i++)
+	{
+		Character lower = tolower(_string[i]);
+		_string[i] = lower;
+	}
+}
+
+void QString::setLower(size_t index)
+{
+	Character ch = at(index);
+	if (!isEmptyChar(ch))
+	{
+		_string[index] = tolower(ch);
+	}
+}
+
+void QString::setLower(size_t begin, size_t end)
+{
+	QBool begCheck = _checkIndex(begin);
+	QBool endCheck = _checkIndex(end);
+	if (begCheck && endCheck)
+	{
+		for (size_t i = begin; i < end + 1; i++)
+		{
+			Character lower = tolower(_string[i]);
+			_string[i] = lower;
+		}
+	}
 }
 
 char& QString::at(size_t index) const
 {
-	return (index <= _length - 1) ? _string[index] : EMPTY_CHAR;
+	return _checkIndex(index) ? _string[index] : EMPTY_CHAR;
+}
+
+QString QString::at(size_t begin, size_t end) const
+{
+	QBool begCheck = _checkIndex(begin);
+	QBool endCheck = _checkIndex(end);
+	if (begCheck && endCheck)
+	{
+		size_t subLen = (end - begin != 0) ? end - begin : 1;
+		QString string(subLen);
+		for (size_t i = 0; i < subLen; i++)
+		{
+			string[i] = _string[i];
+		}
+		return string;
+	}
+	return EMPTY_STRING;
 }
 
 QString::Reference QString::operator[](size_t index) const
@@ -110,9 +260,35 @@ QString::Reference QString::operator[](size_t index) const
 	return at(index);
 }
 
+size_t QString::getLength() const
+{
+	return _length;
+}
+
+size_t QString::getMaxLength() const
+{
+	return _maxLength;
+}
+
+
+size_t QString::getSize() const
+{
+	return _length;
+}
+
+size_t QString::getMaxSize() const
+{
+	return _maxLength;
+}
+
 void QString::_setLength(size_t len)
 {
 	_length = len;
+}
+
+QBool QString::_checkIndex(size_t index) const
+{
+	return index <= _length - 1 && index >= 0;
 }
 
 void QString::_incrementLength()
@@ -127,5 +303,9 @@ void QString::_decrementLength()
 
 std::ostream& operator<<(std::ostream& os, const QString& string)
 {
-	return os << string._string;
+	for (size_t i = 0; i < string.getLength(); i++)
+	{
+		os << string[i];
+	}
+	return os;
 }
