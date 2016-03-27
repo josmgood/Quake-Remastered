dcbec9741a06904cc328f0d3c038b8c04bf57233
Josh <deltaplaythroughs65@gmail.com>
2016-03-27 16:32:23 -0700

    Initial Commit.

---
+++ Quake/UI/Include/Windows/windows_defs.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <windows.h>
+
+typedef PIXELFORMATDESCRIPTOR PixelFormatDescriptor;
+typedef WNDCLASS WndClass;
+typedef HWND Hwnd;
+typedef DWORD DWord;
+typedef MSG Msg;
+typedef LRESULT LResult;
+typedef WPARAM WParam;
+typedef LPARAM LParam;
+typedef HINSTANCE HInstance;
