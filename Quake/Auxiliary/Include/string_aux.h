973b0bfc72b9287a62bd57a80aa05c1e79db5de1
Josh <deltaplaythroughs65@gmail.com>
2016-03-27 16:31:27 -0700

    Removed all C-library custom implementations.

--- Quake/Auxiliary/Include/string_aux.h
+++ Quake/Auxiliary/Include/string_aux.h
@@ -1,64 +1,12 @@
 #pragma once
 
 #include <functional>
+#include <string.h>
 
 #include "..\..\Container\Include\qbool.h"
 #include "..\..\common.h"
 
 static char EMPTY_CHAR = ' ';
-
-struct cString
-{
-	cString(char* str, size_t len);
-
-	char* string;
-	size_t length;
-};
-
-QBool isEmptyChar(char ch);
-QBool isTerminatingZero(char ch);
-
-size_t Q_strLen(const char* string);
-size_t Q_strLen(const char* string, size_t buffer);
-
-void Q_strCpy(const char* src, char* dest);
-void Q_strCpy(const char* src, char* dest, size_t buffer);
-
-char* Q_strCat(const char* src, char* dest);
-char* Q_strCat(const char* src, char* dest, size_t buffer);
-
-QBool Q_isAlpha(char ch);
-QBool Q_isSpace(char ch);
-QBool Q_isNumeric(char ch);
-QBool Q_isUpper(char ch);
-QBool Q_isLower(char ch);
-QBool Q_isHexU(char ch);
-QBool Q_isHexL(char ch);
-
-char Q_toUpper(char ch);
-char Q_toLower(char ch);
-
-int32 Q_toInt32(char ch);
-int32 Q_toInt32(char* string);
-float64 Q_toFloat64(char* string);
-
-QBool Q_chrCmp(char A, char B);
-QBool Q_strCmp(const char* A, const char* B);
-QBool Q_strCmp(const char* A, const char* B, size_t buffer);
-QBool Q_chrCaseCmp(char A, char B);
-QBool Q_strCaseCmp(const char* A, const char* B);
-QBool Q_strCaseCmp(const char* A, const char* B, size_t buffer);
-
-QBool Q_strFind(const char* string, char ch);
-QBool Q_strFind(const char* string, char ch, size_t buffer);
-
-const char* Q_strConstChr(const char* string, char ch);
-const char* Q_strConstChr(const char* string, char ch, size_t buffer);
-
-char* Q_strChr(char* string, char ch);
-char* Q_strChr(char* string, char ch, size_t buffer);
-
-void Q_strPrint(const char* string);
 
 typedef std::function<QBool(char ch)> CharacterFilter;
 typedef std::function<char(char)> CharacterConverter;
@@ -66,11 +14,14 @@ typedef std::function<char(char)> CharacterConverter;
 #define BLANK_FILTER CharacterFilter()
 #define BLANK_CONVERTER CharacterConverter()
 
-#define IS_ALPHA CharacterFilter(Q_isAlpha)
-#define IS_NUMERIC CharacterFilter(Q_isNumeric)
-#define IS_SPACE CharacterFilter(Q_isSpace)
-#define IS_UPPER CharacterFilter(Q_isSpace)
-#define IS_LOWER CharacterFilter(Q_isLower)
+#define IS_ALPHA CharacterFilter(isalpha)
+#define IS_DIGIT CharacterFilter(isdigit)
+#define IS_SPACE CharacterFilter(isspace)
+#define IS_UPPER CharacterFilter(isupper)
+#define IS_LOWER CharacterFilter(islower)
+#define IS_CONTROL CharacterFilter(iscntrl);
+#define IS_PUNCTUATION CharacterFilter(ispunct);
+#define IS_HEXADECIMAL CharacterFilter(isxdigit);
 
-#define TO_UPPER CharacterConverter(Q_toUpper)
-#define TO_SPACE CharacterConverter(Q_toLower)
+#define TO_UPPER CharacterConverter(toupper)
+#define TO_SPACE CharacterConverter(tolower)
