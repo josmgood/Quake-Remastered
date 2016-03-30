750713444a343c13833a6257684a085727e41654
Josh <deltaplaythroughs65@gmail.com>
2016-03-29 21:44:26 -0700

    Added filter and exception functions.

--- Quake/Auxiliary/Include/string_aux.h
+++ Quake/Auxiliary/Include/string_aux.h
@@ -7,14 +7,18 @@
 #include "..\..\common.h"
 
 #define TZERO '\0';
+#define FIRST_INDEX 0
 
 static char EMPTY_CHAR = ' ';
+
+QBool hasTerminatingZero(const char* string);
+QBool hasTerminatingZero(const char* string, size_t length);
 
 QBool chricmp(char one, char two);
 QBool chrcmp(char one, char two);
 
-typedef std::function<QBool(char ch)> CharacterFilter;
-typedef std::function<char(char)> CharacterConverter;
+typedef std::function<bool(char ch)> CharacterFilter;
+typedef std::function<int(char)> CharacterConverter;
 
 #define BLANK_FILTER CharacterFilter()
 #define BLANK_CONVERTER CharacterConverter()
@@ -25,8 +29,14 @@ typedef std::function<char(char)> CharacterConverter;
 #define IS_UPPER CharacterFilter(isupper)
 #define IS_LOWER CharacterFilter(islower)
 #define IS_CONTROL CharacterFilter(iscntrl);
-#define IS_PUNCTUATION CharacterFilter(ispunct);
-#define IS_HEXADECIMAL CharacterFilter(isxdigit);
+#define IS_PUNCTUATION CharacterFilter(ispunct)
+#define IS_HEXADECIMAL CharacterFilter(isxdigit)
 
 #define TO_UPPER CharacterConverter(toupper)
-#define TO_SPACE CharacterConverter(tolower)
+#define TO_LOWER CharacterConverter(tolower)
+
+QBool filterMatch(bool result);
+QBool filterNotMatch(bool result);
+
+QBool exceptionApply(bool result);
+QBool exceptionNotApply(bool result);
--- Quake/Auxiliary/Source/string_aux.cpp
+++ Quake/Auxiliary/Source/string_aux.cpp
@@ -1,5 +1,19 @@
 #include "..\Include\string_aux.h"
 
+QBool hasTerminatingZero(const char* string)
+{
+	const char* ch = string;
+	while (*ch)
+	{
+		if (*(ch + 1) == '\0')
+		{
+			return true;
+		}
+		ch++;
+	}
+	return false;
+}
+
 QBool chricmp(char one, char two)
 {
 	return one == two || one == two - 32 || one == two + 32;
@@ -8,4 +22,24 @@ QBool chricmp(char one, char two)
 QBool chrcmp(char one, char two)
 {
 	return one == two;
+}
+
+QBool filterMatch(bool result)
+{
+	return result == 0;
+}
+
+QBool filterNotMatch(bool result)
+{
+	return result != 0;
+}
+
+QBool exceptionApply(bool result)
+{
+	return result == 0;
+}
+
+QBool exceptionNotApply(bool result)
+{
+	return result != 0;
 }
